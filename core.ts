d5e11601da63375872829d53963d46ec7e2c3091
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-12-19 10:05:08 +0800

    chore: set implementation details as protected chore: specify usage on types from express

--- core.ts
+++ core.ts
@@ -1,11 +1,11 @@
 import { randomUUID } from 'crypto'
-import type express from 'express'
+import type { Request } from 'express'
 
 export class LongPollingTask<Input, Output> {
   constructor(
     public readonly id: string,
     public readonly input: Input,
-    public readonly callback: (output: Output) => void,
+    protected readonly callback: (output: Output) => void,
   ) {}
 
   toJSON() {
@@ -14,21 +14,25 @@ export class LongPollingTask<Input, Output> {
       input: this.input,
     }
   }
+
+  public dispatchResult(output: Output): void {
+    this.callback(output)
+  }
 }
 
 let defaultPollingInterval = 1000 * 30
 
 export class LongPollingTaskQueue<Input, Output> {
-  pollingInterval: number
-  pollingRequests: express.Request[] = []
+  protected pollingInterval: number
+  protected pollingRequests: Request[] = []
 
-  pendingTasks: LongPollingTask<Input, Output>[] = []
+  protected pendingTasks: LongPollingTask<Input, Output>[] = []
 
   constructor(options?: { pollingInterval?: number }) {
     this.pollingInterval = options?.pollingInterval || defaultPollingInterval
   }
 
-  addTask(options: {
+  public addTask(options: {
     id?: string // default random
     input: Input
     callback: (output: Output) => void
@@ -43,18 +47,18 @@ export class LongPollingTaskQueue<Input, Output> {
     return task
   }
 
-  getFirstTask(): LongPollingTask<Input, Output> | null {
+  public getFirstTask(): LongPollingTask<Input, Output> | null {
     if (this.pendingTasks.length == 0) return null
     return this.pendingTasks[0]
   }
 
-  getRandomTask(): LongPollingTask<Input, Output> | null {
+  public getRandomTask(): LongPollingTask<Input, Output> | null {
     if (this.pendingTasks.length == 0) return null
     let index = Math.floor(Math.random() * this.pendingTasks.length)
     return this.pendingTasks[index]
   }
 
-  popTaskById(id: string): LongPollingTask<Input, Output> | null {
+  protected popTaskById(id: string): LongPollingTask<Input, Output> | null {
     let index = this.pendingTasks.findIndex(task => task.id == id)
     if (index === -1) return null
 
@@ -63,7 +67,7 @@ export class LongPollingTaskQueue<Input, Output> {
     return task
   }
 
-  waitTask(req: express.Request) {
+  public waitTask(req: Request) {
     this.pollingRequests.push(req)
     let remove = (cb?: () => void) => {
       let index = this.pollingRequests.indexOf(req)
@@ -82,12 +86,12 @@ export class LongPollingTaskQueue<Input, Output> {
     })
   }
 
-  dispatchResult(
+  public dispatchResult(
     id: string,
     output: Output,
   ): LongPollingTask<Input, Output> | null {
     let task = this.popTaskById(id)
-    task?.callback(output)
+    task?.dispatchResult(output)
     return task
   }
 }
